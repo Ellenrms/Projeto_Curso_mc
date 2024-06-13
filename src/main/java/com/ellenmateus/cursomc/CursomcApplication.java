3bb675549a3c39ef80c3945c6851ef9814bbb9df
Ellen Mateus <ellenrms@gmail.com>
2024-06-13 16:08:23 -0300

    Criando repository e service Categoria

--- src/main/java/com/ellenmateus/cursomc/CursomcApplication.java
+++ src/main/java/com/ellenmateus/cursomc/CursomcApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ellenmateus.cursomc.domain.Categoria;
+import com.ellenmateus.cursomc.domain.Produto;
 import com.ellenmateus.cursomc.repositories.CategoriaRepository;
 
 @SpringBootApplication
@@ -25,8 +26,21 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args ) throws Exception {
 		
-		Categoria cat1 = new Categoria(null,"Informatica");
-		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat1 = new Categoria(null,"Informatica", null);
+		Categoria cat2 = new Categoria(null, "Escritorio", null);
+		
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto (null, "Impressora", 800.00);
+		Produto p3 = new Produto (null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
