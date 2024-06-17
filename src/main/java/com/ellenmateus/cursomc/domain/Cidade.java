81be21187092e529ac302ba5e9b66d018bd309a5
Ellen Mateus <ellenrms@gmail.com>
2024-06-17 15:20:42 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/ellenmateus/cursomc/domain/Cidade.java
+++ src/main/java/com/ellenmateus/cursomc/domain/Cidade.java
@@ -3,6 +3,8 @@ package com.ellenmateus.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Cidade implements Serializable {
 	private String name;
 	
 	
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
--- src/main/java/com/ellenmateus/cursomc/domain/Cliente.java
+++ src/main/java/com/ellenmateus/cursomc/domain/Cliente.java
@@ -8,6 +8,7 @@ import java.util.Objects;
 import java.util.Set;
 
 import com.ellenmateus.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.ElementCollection;
@@ -33,6 +34,8 @@ public class Cliente implements Serializable {
 	private Integer tipo;
 	
 	
+	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")	
 	private List<Endereco> enderecos = new ArrayList<>();
 	
